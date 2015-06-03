9ac734b73dac2ac2e0dda88a805d0e49901623f9
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-06-03 09:54:54 +0200

    further updates

--- code/for.cpp
+++ code/for.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main(){
 	cout << "drie hoeratjes:" << endl;
-	for(int i = 0; i < 3; i++)
+	for(int i = 0; i < 3; i++){
 		cout << "hoera" << endl;
+	}
 
 	cout << "tellen tot 10:" << endl;
-	for(int getal = 0; getal <= 10; getal++)
+	for(int getal = 0; getal <= 10; getal++){
 		cout << getal << endl;
+	}
 
 	return 0;
 }
